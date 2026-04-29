e4175b7e4788193a6e5a258d4b8f771e181d3366
Aslak Johansen <aslakj@gmail.com>
2026-04-29 20:35:05 +0200

    Cleanup

--- src/datadownload.py
+++ src/datadownload.py
@@ -7,6 +7,7 @@ url = "https://data.un.org/_Docs/SYB/CSV/SYB68_1_202511_Population,%20Surface%20
 filename = "popdata.csv"
 
 response = requests.get(url)
+
 if response.status_code == 200:
   with open(filename, "w") as fo:
     fo.writelines([response.text])
