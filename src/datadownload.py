13b272fbaea2a552f4cc3a64a06717a80371f1e7
Aslak Johansen <aslakj@gmail.com>
2026-04-29 20:20:58 +0200

    Added solution for 2nd exercise for part 3

---
+++ src/datadownload.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import requests
+
+url = "https://data.un.org/_Docs/SYB/CSV/SYB68_1_202511_Population,%20Surface%20Area%20and%20Density.csv"
+
+filename = "popdata.csv"
+
+response = requests.get(url)
+if response.status_code == 200:
+  with open(filename, "w") as fo:
+    fo.writelines([response.text])
+else:
+  print("Oops, something went wrong ...")
